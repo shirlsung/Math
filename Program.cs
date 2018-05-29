8eb03c87db0833668ce838192ca3ecca752806da
shirlsung <shaolei.sung@gmail.com>
2018-05-28 18:37:15 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Pick a number to do the following math:");
+            int InputOne = Convert.ToInt32(Console.ReadLine());
+            int result = subMath.SumofTwo(InputOne);
+            int resultOne = subMath.MultbyThree(InputOne);
+            int resultTwo = subMath.divbyTen(InputOne);
+            Console.WriteLine(result);
+            Console.WriteLine(resultOne);
+            Console.WriteLine(resultTwo);
+            Console.ReadLine();
+        }
+        
+            
+
+        }
+}
---
+++ subMath.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math
+{
+    class subMath
+    {
+        public static int SumofTwo (int InputOne)
+        {
+            return InputOne*2;
+        }
+        public static int MultbyThree(int InputOne)
+        {
+            return InputOne*3;
+        }
+        public static int divbyTen(int InputOne)
+        {
+            return InputOne / 10;
+        }
+    }
+   
+}
